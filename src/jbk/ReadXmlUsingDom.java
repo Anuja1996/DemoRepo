39ee2408057412357161080975294e128d2b8baf
Anuja <avanivyavahare@gmail.com>
2019-05-21 13:31:58 +0530

    changes in ReaderSax

--- src/jbk/ReadXmlUsingDom.java
+++ src/jbk/ReadXmlUsingDom.java
@@ -11,33 +11,35 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class ReadXmlUsingDom {
+
+	//Con
 	public static void main(String[] args) throws Exception{
 		try{
 	    File fxml=new File("XmlDoc.xml");
 		DocumentBuilderFactory dbfactory=DocumentBuilderFactory.newInstance();
 		DocumentBuilder b=dbfactory.newDocumentBuilder();
 		Document doc=b.parse(fxml);
-		
+
 		System.out.println("Root element:" + doc.getDocumentElement().getNodeName());
 		NodeList nList=doc.getElementsByTagName("students");
 		System.out.println("-------" + nList.getLength());// legnth will calculate no of elements of xml file to print
 		for(int temp=0;temp<nList.getLength();temp++)
 		{
 			Node nNode=nList.item(temp);
-			
+
 			System.out.println("\nCurrent Element:" +nNode.getNodeName());
-			 
+
 			if(nNode.getNodeType()==Node.ELEMENT_NODE){
 				Element eElement=(Element) nNode;
-	
+
 				System.out.println("name:" + eElement.getElementsByTagName("name").item(0).getTextContent());
 				System.out.println("email:" + eElement.getElementsByTagName("email").item(0).getTextContent());
 				System.out.println("mobile:" + eElement.getElementsByTagName("mobile").item(0).getTextContent());
 				System.out.println("address:" + eElement.getElementsByTagName("address").item(0).getTextContent());
-				
+
 			}
 		}
-				
+
 	}catch(Exception e){
 		e.printStackTrace();
 
